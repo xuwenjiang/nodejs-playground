7ceb101e0708cdb01865f1a1446448c85f4e4281
wenjiang.xu <wenjiang.xu@missguided.com>
2020-08-24 15:58:34 +0100

    add some comment

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
+// import function and call it
 import {sayHello} from './hello.js';
-
 sayHello();
 
+// define arrow function a
 const a = (param, param2) => {
   console.log('arrow ' + param2);
 }
 
+// define arrow function b and call it
 const b = w => console.log(w);
 
 b('See? I am w');
 
+// set a callback, it is ut in the "Message Queue"
 setTimeout(() => {console.log('timeout')}, 0);
 
+// ECMAScript 2015 introduced "Job Queue", which is used by Promises (will exectute as soon as possible)
+// so it is before above callback.  and that is a big difference
 let done = false;
-
 const isItDoneYet = new Promise((resolve, reject) => {
   if (done) {
     const workDone = 'Here is the thing I built'
@@ -36,4 +40,5 @@ const checkIfItsDone = () => {
 
 checkIfItsDone()
 
+// call previously defined arrow function a.
 a(1,2);
